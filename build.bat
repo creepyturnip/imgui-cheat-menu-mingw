2c4d57cd5e54096221f157c80a962cd76ea9356e
creepyturnip <creepyturnip@gmail.com>
2023-12-04 12:51:35 +0100

    made the gui rendering a seperate file so the main file still looks kind off clean yknow also i made the window toggleable with INSERT

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-g++ -std=c++20 -o gui .\main.cpp .\imgui\*.cpp .\imgui\*.h -I./imgui -I./GLFW/include -L./GLFW/lib-mingw-w64 -lglfw3 -lopengl32 -lgdi32 -limm32 -lshell32 -lole32 -lpthread
+g++ -std=c++20 -o gui .\*.cpp .\*.hpp .\imgui\*.cpp .\imgui\*.h -I./imgui -I./GLFW/include -L./GLFW/lib-mingw-w64 -lglfw3 -lopengl32 -lgdi32 -limm32 -lshell32 -lole32 -lpthread
---
+++ gui.hpp
@@ -0,0 +1,113 @@
+#include <Windows.h>
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+#include <stdio.h>
+#define GL_SILENCE_DEPRECATION
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include "GLFW/\include\glfw3.h"
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+#ifdef __EMSCRIPTEN__
+#include "../libs/emscripten/emscripten_mainloop_stub.h"
+#endif
+
+constexpr int imguiWIDTH = 400;
+constexpr int imguiHeight = 300;
+
+bool focussed = true;
+
+static void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+// Main code
+int createWindowWithImgui(int windowWith, int windowHeight, int maximized, int decorated)
+{
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, 1);
+    glfwWindowHint(GLFW_MAXIMIZED, maximized);
+    glfwWindowHint(GLFW_DECORATED, decorated);
+
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(windowWith, windowHeight, "Imgui window", nullptr, nullptr);
+    if (window == nullptr)
+        return 1;
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    //style
+    ImGui::StyleColorsDark();
+    ImGuiStyle& style = ImGui::GetStyle();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        if (GetAsyncKeyState(VK_INSERT) & 0x01) {
+            focussed = !focussed;
+            if (focussed) {
+                glfwRestoreWindow(window);
+            } else {
+                glfwIconifyWindow(window);
+            }
+        }
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::SetNextWindowSize({imguiWIDTH, imguiHeight});
+        ImGui::NewFrame();
+        {
+            ImGui::Begin("Hello, Imgui!");
+            ImGui::End();
+        }
+
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
